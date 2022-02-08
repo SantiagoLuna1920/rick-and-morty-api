8179bb6c85a8e84beb2fbb523d58ef71de94bb61
Santiago Luna <kobyj103@gmail.com>
2022-02-08 00:43:32 -0500

    Agregado act

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import "./js/initGenerator";
 import { init } from "./js/initGenerator";
 import "/src/styles.css";
 import "/src/css/components.css";
+import "./js/api/petitions.api";
+import "./js/html-api.generator";
+import "./js/html-form.generator";
+
 
 
 
 init();
-
-console.log("Hola");
